5955e5e82009f003787b7e67db1794f7fa980dd4
lernEarnBurn <lerners124@gmail.com>
2024-01-21 13:58:09 +0200

    updated user schema

--- api/models/user.js
+++ api/models/user.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  description: { type: String, required: true, default: 'Hello I am here using Wheresapp.' },
+  profilePic: { type: Schema.Types.ObjectId, ref: 'Image', default: null }
 });
 
 module.exports = mongoose.model("User", userSchema);
