8b3a50a8e632098cf230b2115a49c9263dc9a310
lernEarnBurn <lerners124@gmail.com>
2024-01-21 15:45:09 +0200

    tweaked schema

--- api/models/message.js
+++ api/models/message.js
@@ -4,12 +4,14 @@ const Schema = mongoose.Schema
 const messageSchema = new Schema({
   timestamp: {
     type: Date,
-    required: true
+    required: true,
+    default: Date.now()
   },
   content: {
     type: Schema.Types.Mixed,
     required: true
-  }
+  },
+  sender: {type: Schema.Types.ObjectId, ref: 'User', required: true}
 })
 
 module.exports = mongoose.model('Message', messageSchema)
