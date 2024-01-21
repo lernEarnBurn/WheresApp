b759f112e85ec36fcc03d408240c0e0dfea7a98c
lernEarnBurn <lerners124@gmail.com>
2024-01-21 14:51:59 +0200

    added method that creates conversation

--- api/controllers/conversationController.js
+++ api/controllers/conversationController.js
@@ -3,5 +3,24 @@ const asyncHandler = require('express-async-handler')
 const Conversation = require('../models/conversation')
 
 exports.createConversation = asyncHandler(async(req, res, next) => {
+    try{
+        const conversation = new Conversation({
+            users: [req.params.userId, req.body.recipient]
+        })
+
+        await conversation.save()
+
+        res.json(conversation)
+    } catch(err){
+        res.status(500)
+        console.log(err)
+    }
+})
+
+exports.getSingleConversation = asyncHandler(async(req, res, next) => {
+    res.json('')
+})
+
+exports.getAllConversations = asyncHandler(async(req, res, next) => {
     res.json('')
 })
