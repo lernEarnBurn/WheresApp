0cfa2348205b0336b00fbb648fa1f2d01d029e81
lernEarnBurn <lerners124@gmail.com>
2024-01-21 15:45:33 +0200

    made method that creates message

--- api/controllers/messageController.js
+++ api/controllers/messageController.js
@@ -1,7 +1,26 @@
 const asyncHandler = require('express-async-handler')
 
 const Message = require('../models/message')
+const Conversation = require('../models/conversation')
 
 exports.createMessage = asyncHandler(async(req, res, next) => {
-    res.json('')
+    try{
+        const message = new Message({
+            content: req.body.content,
+            sender: req.params.userId
+        })
+
+        await Conversation.findByIdAndUpdate(
+            req.params.conversationId,
+            { $push: { messages: message._id } },
+            { new: true } 
+        );
+        
+        await message.save()
+        res.json(message)
+    } catch(err){
+        console.log(err)
+        res.status(500).json({error: 'Server Error.'})
+
+    }
 })
