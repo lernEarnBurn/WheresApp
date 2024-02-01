97e055c6ae9808b23a551091d7549caa7338062e
lernEarnBurn <lerners124@gmail.com>
2024-02-01 15:26:26 +0200

    structured user settings

--- frontend/src/components/UserSettings.jsx
+++ frontend/src/components/UserSettings.jsx
@@ -1,5 +1,8 @@
 export function UserSettings(){
   return (
-    <section className='bg-black w-[30%] h-full'></section>
+    <section className='bg-black w-[30%] h-full flex flex-col'>
+      <div className="bg-blue-500 w-full h-[18%]"></div>
+      <div className="bg-green-500 w-full h-[82&]"></div>
+    </section>
   )
 }
