59832d09ca770fe8f8b951eb59d89823d5813ca0
lernEarnBurn <lerners124@gmail.com>
2024-02-01 15:31:03 +0200

    structured convInterface

--- frontend/src/components/ConvInterface.jsx
+++ frontend/src/components/ConvInterface.jsx
@@ -1,5 +1,9 @@
 export function ConvInterface(){
   return (
-    <section className='bg-green-300 w-[70%] h-full'></section>
+    <section className='w-[70%] h-full'>
+      <div className="w-full h-[9%] bg-blue-500"></div>
+      <div className="w-full h-[81%] bg-yellow-500"></div>
+      <div className="w-full h-[10%] bg-red-500"></div>
+    </section>
   )
 }
