b722017070fb1a71a703ac747e2c3ac1f8ea5149
lernEarnBurn <lerners124@gmail.com>
2024-02-01 15:23:30 +0200

    structured convColumn

--- frontend/src/components/ConvColumn.jsx
+++ frontend/src/components/ConvColumn.jsx
@@ -1,6 +1,13 @@
+import { SearchBar } from "./SearchBar"
+
 export function ConvColumn(){
 
   return (
-    <section className='bg-black w-[30%] h-full'></section>
+    <section className='w-[30%] h-full flex flex-col'>  
+      <div className="w-full h-[10%] bg-green-500"></div>
+      <SearchBar/>
+      <div className="w-full h-[82%] bg-red-500"></div>
+    
+    </section>
   )
 }
---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,5 @@
+export function SearchBar(){
+  return (
+    <section className="w-full h-[8%] bg-blue-500"></section>
+  )
+}
