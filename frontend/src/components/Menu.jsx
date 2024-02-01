ee76f750082c47ef1cddf5408b3ffc4d7d577087
lernEarnBurn <lerners124@gmail.com>
2024-02-01 14:24:38 +0200

    added logout functionality for testing purposes

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,5 +1,24 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 export function Menu(){
+  const navigate = useNavigate()
+  
+  const [user] = useState(localStorage.getItem('user'))
+
+
+  function logOut(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    navigate('/WheresApp/Login')
+  }
+
   return (
-    <>main menu</>
+    <>
+      <p>{user}</p>
+      <button onClick={logOut}>Logout</button>
+    </>
+
   )
 }
