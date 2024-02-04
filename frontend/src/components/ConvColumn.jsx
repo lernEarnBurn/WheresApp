bb7207c39d22ad5dd0321b73e2c35b45f11d9fec
lernEarnBurn <lerners124@gmail.com>
2024-02-04 15:56:54 +0200

    added hook that gets all conversations a person has

--- frontend/src/components/ConvColumn.jsx
+++ frontend/src/components/ConvColumn.jsx
@@ -1,13 +1,54 @@
+import { useState, useEffect } from "react"
 import { SearchBar } from "./SearchBar"
 
+import axios from 'axios'
+
 export function ConvColumn(){
+  const { conversations } = useGetConversations()
+
+  //need to have the person who's not u in conversation's name added tp the data object
+  //backend issue
+
+  //also need to populate lastMessage
 
   return (
     <section className='w-[30%] h-full flex flex-col'>  
       <div className="w-full h-[10%] bg-green-500"></div>
       <SearchBar/>
-      <div className="w-full h-[82%] bg-red-500"></div>
+      <div className="w-full h-[82%] flex flex-col overflow-y-scroll">
+        {conversations.map((conv, index) => (
+          <div key={index} className="w-full h-[15%] bg-red-200">
+            <h1>{conv.users}</h1>
+            <h2>{conv.lastMessage}</h2>
+          </div>
+        ))}
+        
+      </div>
     
     </section>
   )
+}
+
+function useGetConversations(){
+  const [conversations, setConversations] = useState([])
+
+  useEffect(() => {
+    const convRequest = async() => {
+      try {
+        const response = await axios.get(`http://localhost:3000/user/${JSON.parse(localStorage.getItem('user'))._id}/conversations`, {
+                                            headers: {
+                                              'Authorization': `Bearer ${localStorage.getItem('token')}`
+                                            }
+                                          })
+        setConversations(response.data)
+      } catch (err){
+        console.log(err)
+      }
+    }
+
+    convRequest()
+  }, [])
+
+  return { conversations }
+
 }
