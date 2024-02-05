cacff68124295765225c2ab05ae638e6988d5a0e
lernEarnBurn <lerners124@gmail.com>
2024-02-05 19:15:23 +0200

    search bar styling

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,5 +1,10 @@
+
+
 export function SearchBar(){
   return (
-    <section className="w-full h-[8%] bg-blue-500"></section>
+    <section className="w-full h-[8%] grid place-items-center  border-gray-300 border-b border-t">
+      <input type="text" className="search-bar h-8 w-[90%] rounded-2xl"></input>
+      <svg className="svg"></svg>
+    </section>
   )
 }
