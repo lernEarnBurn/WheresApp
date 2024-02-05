a85d6bcf543fbd3d0735477432e1239a17ddf7c7
lernEarnBurn <lerners124@gmail.com>
2024-02-05 19:36:11 +0200

    structured and added profile pic

--- frontend/src/components/ConvColumn.jsx
+++ frontend/src/components/ConvColumn.jsx
@@ -12,13 +12,27 @@ export function ConvColumn(){
 
   return (
     <section className='w-[30%] h-full flex flex-col'>  
-      <div className="w-full h-[10%] bg-green-500"></div>
+      <div className="w-full h-[10%]"></div>
       <SearchBar/>
       <div className="w-full h-[82%] flex flex-col overflow-y-scroll">
         {conversations.map((conv, index) => (
-          <div key={index} className="w-full h-[15%] bg-red-200">
-            <h1>{conv.users[0].username}</h1>
-            <h2>{conv.lastMessage}</h2>
+          <div key={index} className="w-full h-[15%] border-b py-4 px-3 border-gray-300 flex items-center gap-3 hover-bg">
+            {conv.users[0].profilePic ? (
+              <div className="w-10 h-10 rounded-full overflow-hidden">
+                <img
+                  src={conv.users[0].profilePic}
+                  alt={`${conv.users[0].username}'s profile`}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            ) : (
+              <div className="w-16 h-16 default-pic"></div>
+            )}
+
+            <div className="flex flex-col">
+              <h1 className="text-lg font-semibold">{conv.users[0].username}</h1>
+              <h2 className="text-gray-600">{/*conv.users[0].lastMessage*/} This is a dummy last Message</h2>
+            </div>
           </div>
         ))}
         
