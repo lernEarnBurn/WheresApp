93e999d14769bb2398cb3b226117ffbbcaf17de2
lernEarnBurn <lerners124@gmail.com>
2024-02-20 18:54:37 +0200

    set up styles for message bubble

--- frontend/src/components/ConvInterface.jsx
+++ frontend/src/components/ConvInterface.jsx
@@ -5,6 +5,9 @@ import { MoreVertical } from 'lucide-react';
 
 import { motion } from 'framer-motion'
 
+//i think the conversation obj is gonna be in a context that when click on contact or current conv then
+//it will populate context with what it needs and that will display everything on this screen
+
 export function ConvInterface(){
   const buttonVariants = {
     rest: { scale: 1 },
@@ -14,7 +17,6 @@ export function ConvInterface(){
   return (
     <section className='w-[70%] h-full'>
       <div className="w-full h-[9%] flex gap-3 items-center border-x border-gray-300">
-        
         <div className="ml-5 w-14 h-14 default-pic flex-shrink-0"></div>
         <div className='flex flex-col mb-1  overflow-hidden max-w-[23vw]'>
           <h1 className='text-lg truncate font-semibold'>Contacts Name</h1>
@@ -22,7 +24,14 @@ export function ConvInterface(){
         </div>
         <MoreVertical className='ml-[45.5vw]'/>
       </div>
-      <div className="w-full h-[81%] user-input-bg border border-b-0  border-gray-300"></div>
+
+
+      <div className="w-full h-[81%] flex flex-col gap-5 user-input-bg border border-b-0 py-10 px-16 border-gray-300 overflow-y-scroll">
+        <div className="message">Hello! This is a sent message.</div>
+        <div className="message received">And this is a reply.</div>
+      </div>
+
+
       <div className="w-full h-[10%] flex items-center gap-4 border border-l-0 border-gray-300">
         <motion.button variants={buttonVariants} whileHover="hover" whileTap="rest"  initial="rest" className='ml-4 rounded-full'><Image/></motion.button>
         <motion.button variants={buttonVariants} whileHover="hover" whileTap="rest"  initial="rest" className='rounded-full'><SmilePlus/></motion.button>
