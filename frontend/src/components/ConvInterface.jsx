5bde8a825ef2fc6f77ed4d36a0043f98b1299cc5
lernEarnBurn <lerners124@gmail.com>
2024-02-20 18:22:26 +0200

    styled rest of section

--- frontend/src/components/ConvInterface.jsx
+++ frontend/src/components/ConvInterface.jsx
@@ -1,6 +1,7 @@
 import { SendHorizontal } from 'lucide-react';
 import { Image } from 'lucide-react';
 import { SmilePlus } from 'lucide-react';
+import { MoreVertical } from 'lucide-react';
 
 import { motion } from 'framer-motion'
 
@@ -12,8 +13,16 @@ export function ConvInterface(){
   
   return (
     <section className='w-[70%] h-full'>
-      <div className="w-full h-[9%] bg-blue-500"></div>
-      <div className="w-full h-[81%] bg-yellow-500"></div>
+      <div className="w-full h-[9%] flex gap-3 items-center border-x border-gray-300">
+        
+        <div className="ml-5 w-14 h-14 default-pic flex-shrink-0"></div>
+        <div className='flex flex-col mb-1  overflow-hidden max-w-[23vw]'>
+          <h1 className='text-lg truncate font-semibold'>Contacts Name</h1>
+          <h2 className='text-xs truncate'>This is my description and it is long</h2>
+        </div>
+        <MoreVertical className='ml-[45.5vw]'/>
+      </div>
+      <div className="w-full h-[81%] user-input-bg border border-b-0  border-gray-300"></div>
       <div className="w-full h-[10%] flex items-center gap-4 border border-l-0 border-gray-300">
         <motion.button variants={buttonVariants} whileHover="hover" whileTap="rest"  initial="rest" className='ml-4 rounded-full'><Image/></motion.button>
         <motion.button variants={buttonVariants} whileHover="hover" whileTap="rest"  initial="rest" className='rounded-full'><SmilePlus/></motion.button>
